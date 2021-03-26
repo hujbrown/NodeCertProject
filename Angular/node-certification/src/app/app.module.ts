1b9f9d53ac144a4bcc608902e64a3e732c11831a
Hunter Brown <hujbrown@ucsc.edu>
2021-03-26 08:45:28 -0700

    Created components and services

--- Angular/node-certification/src/app/app.module.ts
+++ Angular/node-certification/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
