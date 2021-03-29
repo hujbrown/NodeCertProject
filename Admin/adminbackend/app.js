212c4615adbd9137d165d4727cd73371a0e4e695
hitixapatel07 <hitixapatel07@gmail.com>
2021-03-29 16:36:29 -0400

    half merging backend
    api and cors added

--- Admin/adminbackend/app.js
+++ Admin/adminbackend/app.js
@@ -3,16 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const adminRoutes = require('./routes/routeAdmin');
 const authRoutes = require('./routes/routeAuth');
-
+const newsRoutes = require('./routes/routeNews');
 const userRoutes = require('./routes/routeUser');
 var homepageRouter = require('./routes/homepage');
 var addRouter = require('./routes/add')
+var newsListRouter =require('./routes/api')
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -53,15 +56,16 @@ app.get('/register', (req, res) => {
       res.render('register', {errorMsg: null, successMsg: null, isLoggedIn: false}); // Landing Page
   }
 });
-
-app.use('/admin', adminRoutes);
-app.use('/api/auth', authRoutes);
-
-app.use('/api/users', userRoutes);
-//app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/homepage', homepageRouter);
 app.use('/add', addRouter);
+app.use('/api', newsListRouter);
+app.use('/admin', adminRoutes);
+app.use('/api/sec/auth', authRoutes);
+app.use('/api/sec/news', newsRoutes);
+app.use('/api/sec/users', userRoutes);
+//app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
